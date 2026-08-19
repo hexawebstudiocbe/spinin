a64c2f53f4b0bf88280f2d35661dacd78cea2ad8
varun-it21 <varunrohith626@gmail.com>
2026-08-19 14:54:14 +0530

    feat: add favicon and apple-touch-icon assets with updated conversion script and document references

--- scratch_convert.ps1
+++ scratch_convert.ps1
@@ -1,6 +1,14 @@
 Add-Type -AssemblyName System.Drawing
 $img = [System.Drawing.Image]::FromFile("d:\Varun\Spinin\src\assets\SPIN IN Logo.jpg.jpeg")
-$bmp = new-object System.Drawing.Bitmap $img, 192, 192
-$bmp.Save("d:\Varun\Spinin\public\favicon.png", [System.Drawing.Imaging.ImageFormat]::Png)
-$bmp.Dispose()
+
+# apple-touch-icon.png (180x180)
+$bmpApple = new-object System.Drawing.Bitmap $img, 180, 180
+$bmpApple.Save("d:\Varun\Spinin\public\apple-touch-icon.png", [System.Drawing.Imaging.ImageFormat]::Png)
+$bmpApple.Dispose()
+
+# favicon.ico (48x48 - PNG encoded but .ico extension is widely supported by browsers)
+$bmpIco = new-object System.Drawing.Bitmap $img, 48, 48
+$bmpIco.Save("d:\Varun\Spinin\public\favicon.ico", [System.Drawing.Imaging.ImageFormat]::Png)
+$bmpIco.Dispose()
+
 $img.Dispose()
